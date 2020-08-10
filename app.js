5260c4ae6380551b276dae9fc443b7c6608a3555
Malik1689 <faizankhan.dae@gmail.com>
2020-08-10 15:08:44 +0500

    facebook

---
+++ app.js
@@ -0,0 +1,13 @@
+function data(){
+
+
+var name=document.getElementById("name").value
+var pass=document.getElementById("pass").value
+
+var data={
+    name:name,
+    pass:pass,
+}
+console.log(data.name)
+
+}
